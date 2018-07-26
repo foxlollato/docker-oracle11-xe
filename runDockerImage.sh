8fafa4d5b9427f950c759278d49c75b443db2972
vitor.lollato <vitor.lollato@itbox.online>
2018-07-26 19:30:04 -0300

    first commit

---
+++ runDockerImage.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker run \
+--shm-size=2g \
+-p 1521:1521 -p 8080:8080 \
+-v /home/vitor/docker-volumes/oradata/:/u01/app/oracle/oradata \
+-e ORACLE_PWD=$1 \
+oracle/database:11.2.0.2-xe
+
